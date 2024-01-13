3ba1e06351e2818dc625a8ec7c0feebade95ef2c
griffinryan <glryan@uw.edu>
2024-01-12 17:26:23 -0800

    different approach

--- src/Worm.java
+++ src/Worm.java
@@ -1,58 +1,35 @@
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 
 public class Worm {
-    int numOfTimes;
-    String[] array = {
-            "public class Worm2 {",
-            "int numOfTimes",
-            "String[] array ={",
-            "};",
-            "public static void main(String[] args){",
-            "Worm worm = new Worm();",
-            "char quote = 34;",
-            "System.out.println(worm.array[0]);",
-            "System.out.println(worm.array[1]+'='+ (worm.numOfTimes+1)+';');",
-            "System.out.println(worm.array[2]);",
-            "for (int i = 0; i < worm.array.length; i++) {",
-            "System.out.println(quote+worm.array[i]+quote+',');",
-            "}",
-            "for (int i = 2; i <worm.array.length; i++) {",
-            " System.out.println(worm.array[i]);",
-            "}",
-            "}",
-            "}",
-    };
+    public static void main(String[] args) throws IOException {
+        int replicationCount = args.length > 0 ? Integer.parseInt(args[0]) : 5;
 
-    public static void main(String[] args) {
-        Worm worm = new Worm();
-        char quote = 34;
-        if (worm.numOfTimes < 3) {
-            try {
-
-                PrintWriter myWriter = new PrintWriter("src/Worm2.java");
-                myWriter.println(worm.array[0]);
-                myWriter.println(worm.array[1] + '=' + (worm.numOfTimes + 1) + ';');
-                myWriter.println(worm.array[2]);
-                for (int i = 0; i < worm.array.length; i++) {
-                    myWriter.println(quote + worm.array[i] + quote + ',');
-                }
-                for (int i = 3; i < worm.array.length; i++) {
-                    myWriter.println(worm.array[i]);
-                }
-                myWriter.close();
-                Process i = Runtime.getRuntime().exec("javac " + "Worm2" + ".java");
-                i.waitFor();
-                i = Runtime.getRuntime().exec("java " + "Worm2");
-
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+        if (replicationCount > 0) {
+            String newFileName = "Worm" + ".java";
+            String newDirectoryName = "src/dir" + replicationCount;
+            File newDir = new File(newDirectoryName);
+            if (!newDir.exists()) {
+                newDir.mkdir();
             }
+
+            File newFile = new File(newDir, newFileName);
+            try (BufferedReader reader = new BufferedReader(new FileReader("src/Worm.java"));
+                 PrintWriter writer = new PrintWriter(new FileWriter(newFile))) {
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    writer.println(line);
+                }
+            }
+
+            // Update the replication count for the next iteration
+            String newArgs = String.valueOf(replicationCount - 1);
+
+            // Compile the new file
+            Runtime.getRuntime().exec("javac " + newFile.getPath());
+
+            // Execute the new file
+            Runtime.getRuntime().exec("java -cp " + newDirectoryName + " Worm ");
         }
     }
 }
